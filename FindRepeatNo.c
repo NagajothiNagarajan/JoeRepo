d05da3275930566103e97a56beea11364bd6887b
NagajothiNagarajan <31894105+NagajothiNagarajan@users.noreply.github.com>
2017-09-13 04:47:55 +0530

    Finding Repeated nos

---
+++ FindRepeatNo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int size;
+    scanf("%d",&size);
+    
+    int no_array[size];
+    for(int i=0;i<size;i++)
+        scanf("%d",&no_array[i]);
+    
+    for(int i=0;i<size;i++)    
+    {
+        if(no_array[abs(no_array[i])] >= 0)
+            no_array[abs(no_array[i])] = -no_array[abs(no_array[i])];
+        else
+            printf("%d\t",abs(no_array[i]));
+    }
+    return 0;
+}
